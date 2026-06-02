96fa8b457807faa01f2a0b783a30e739f2225314
juansalas <juan10zmgh@gmail.com>
2026-06-02 16:55:19 -0300

    Agregue algunas cosas de javascript

---
+++ javascript/Leccion03/03-01-OperadoresJs.js
@@ -0,0 +1,17 @@
+// Ejercitacion: Encontrar numeros pares e inpares
+let parInpar = 8;
+if (parInpar % 2 == 0){
+    console.log("El numero es par");
+}
+else {
+    console.log("El numero es inpar");
+}
+
+// Ejercitacion: Mayor de edad
+let edad = 21, mayor = 18;
+if (edad >= mayor){
+    console.log("Es mayor de edad: " + edad);
+}
+else {
+    console.log("Es menor de edad: " + edad);
+}
